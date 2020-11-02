c6ccb692409bab085626702669bb9f356b44f053
Daniel <danielelielgaio@gmail.com>
2020-11-02 19:52:18 -0300

    Adição de comentarios

--- fibonacci.sv
+++ fibonacci.sv
@@ -1,24 +1,39 @@
-module fibonacci(input clk, clr, output [13:0]f);
+
+// O módulo possui dois sinais de entrada (1 bit cada) e um de saida (14 bits)
+module fibonacci(input clk, reset, output [13:0]f);
 	
+	// registradores para armazenar os valores da soma atual e da anterior
   	reg [13:0] fn1, fn;
 	
+	// valor até o qual a sequencia será calculada (corresponde a sétima saida)
 	parameter fib_max = 21;
 		
-	// com reset assincrono
+	// with assincronous reset
 	always @(posedge clk) begin
-		if (clr == 1) begin
+
+		if (reset == 1) begin
+
+			// no reset são carregados os primeiros valores a serem somados
 			fn <= 0;
 			fn1 <= 1;
+
 		end else begin
+
+			// vendo se o ultimo valor somado não ultrapassa o limite estipulado
 			if (fn1 <= fib_max) begin
 				fn1 <= fn1 + fn;
 				fn <= fn1;
+
+			// se o limite for alcançado os registradores são reinicializados
 			end else begin
 				fn <= 0;
 				fn1 <= 1;
 			end
+
 		end
 	end
+
+	// direcionando resultado da soma para o pino de saida
 	assign f = fn;
 	
 endmodule
--- fibonacci_tb.sv
+++ fibonacci_tb.sv
@@ -1,41 +1,44 @@
-// Code your testbench here
-// or browse Examples
 
 module fibonacci_tb;
 
   // inputs of dut as reg
-  reg clk_tb, clr_tb;
+  reg clk_tb, reset_tb;
 
   // outputs of dut as wire
   wire [13:0]f_tb;
 
+
+  // instanciando o modulo criado
   fibonacci my_fib_dut (
     .clk(clk_tb),
-    .clr(clr_tb),
-	 .f(f_tb)
+    .reset(reset_tb),
+    .f(f_tb)
   );
 
+
+  // enviando os primeiros sinais para a arquitetura
   initial begin
     clk_tb = 0;
-    clr_tb = 0;
-    clk_tb = 0;
+    reset_tb = 1;
   end
 
-  always
-    #5 clk_tb = ~clk_tb;
+  // bloco gerador de clock com periodo de 10 unidades de tempo
+  always #5 clk_tb = ~clk_tb;
+
 
   initial begin
-    $dumpfile("fibonacci.vcd");
-    $dumpvars;
+
+    #2 reset_tb = 0;
+
+    // exibindo no console textual os valores de todos os sinais, sempre que houver uma mudança em algum deles
+    $display("\t clk_tb, \t reset_tb, \t fibonacci value");
+    $monitor("\t %b, \t %b, \t %d", clk_tb, reset_tb, f_tb);
+
   end
 
+  // after 95 time units we have 7 outputs
   initial begin
-    //$display("\t clk_tb, \t clr_tb, \t f_tb");
-    $monitor("\t %b, \t %b, \t %d", clk_tb, clr_tb, f_tb);
-  end
-
-  initial begin
-    #100 $finish;
+    #95 $stop;
   end
   
   
